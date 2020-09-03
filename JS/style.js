dffe664a618cfe2065dc4d3041e700551867d777
Bikash Ghale <ghalebikash445@gmail.com>
2020-09-03 19:58:58 +0545

    full website

---
+++ JS/style.js
@@ -0,0 +1,9 @@
+function displayMenu(){
+	let menu = document.getElementById("menu");
+	if (menu.style.display === "none"){
+		menu.style.display = "block";
+	} else{
+		menu.style.display = "none";
+	}
+}
+	
